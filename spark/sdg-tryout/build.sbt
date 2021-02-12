d9ec590440e6b6f26fab16e22e8e1962166b8f3e
Alejandro <agalvez@stratio.com>
2021-02-12 19:40:42 +0100

    Added scala/spark project. Added dags and improved docker-compose

---
+++ spark/sdg-tryout/build.sbt
@@ -0,0 +1,9 @@
+name := "sdg-tryout"
+
+version := "0.1"
+
+scalaVersion := "2.12.10"
+
+idePackagePrefix := Some("sdg.tryout")
+
+libraryDependencies += "org.apache.spark" %% "spark-sql" % "3.0.1"
---
+++ spark/sdg-tryout/src/main/scala/App.scala
@@ -0,0 +1,15 @@
+package sdg.tryout
+
+import org.apache.spark.sql.SparkSession
+
+object App {
+  def main(args: Array[String]): Unit = {
+    val logFile = ".gitignore" // Should be some file on your system
+    val spark = SparkSession.builder.appName("Simple Application").getOrCreate()
+    val logData = spark.read.textFile(logFile).cache()
+    val numAs = logData.filter(line => line.contains("a")).count()
+    val numBs = logData.filter(line => line.contains("b")).count()
+    println(s"Lines with a: $numAs, Lines with b: $numBs")
+    spark.stop()
+  }
+}
